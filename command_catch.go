dadcee3cb145cc72b855a034b1165e7a7b05ae2f
vossfolke <129680876+vossfolke@users.noreply.github.com>
2024-04-04 08:53:42 +0000

    added Pokedex

---
+++ command_catch.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math/rand/v2"
+)
+
+func commandCatch(cfg *config, args ...string) error {
+
+	pokemon, err := cfg.pokeapiClient.CatchPokemon(args[0])
+	if err != nil {
+		return err
+	}
+
+	// add catch func
+	maxBaseExp := 608.0
+
+	chance := (1 - (float64(pokemon.BaseExperience))/maxBaseExp) * rand.Float64()
+
+	fmt.Print("A wild %s appeared!\n", args[0])
+	fmt.Printf("Throwing a Pokeball at %s...\n", args[0])
+	if chance < 0.5 {
+		fmt.Printf("%s escaped!\n", args[0])
+	} else {
+		fmt.Printf("%s was caught!\n", args[0])
+		
+	}
+	 return nil
+}
---
+++ internal/pokeapi/catch_pkmn.go
@@ -0,0 +1,45 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func (c *Client) CatchPokemon(name string) (Pokemon, error) {
+	url := baseURL + "/pokemon/" + name
+
+	if entry, ok := c.cache.Get(url); ok {
+		pokemon := Pokemon{}
+		err := json.Unmarshal(entry, &pokemon)
+		if err != nil {
+			return Pokemon{}, err
+		}
+		return pokemon, nil
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return Pokemon{}, err
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return Pokemon{}, err
+	}
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return Pokemon{}, err
+	}
+
+	pokemon := Pokemon{}
+	err = json.Unmarshal(data, &pokemon)
+	if err != nil {
+		return Pokemon{}, err
+	}
+
+	c.cache.Add(url, data)
+	return pokemon, nil
+}
